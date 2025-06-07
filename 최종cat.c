f6c81f0a17cc8270a27b16da39014c8cfd976019
yty1120 <yty8119@gmail.com>
2025-06-08 06:39:54 +0900

    2-5 상호작용 입력
    장난감이없는경우
    장난감이둘다있는경우

--- 최종cat.c
+++ 최종cat.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <windows.h>
+#include <windows.h> 
 
 #define ROOM_WIDTH 15
 #define HME_POS 1
@@ -10,7 +10,8 @@
 
 void printGameState(const char* name, int soup_count, int affinity, int cp, int mood);
 void drawRoom(int cat_x, int prevPos, int hasScratcher, int hasTower);
-void performAction(int cat_x, int* mood, int* soup_count, int* cp, const char* name, int hasScratcher, int hasTower); // 행동 처리
+void openShop(int* cp, int* hasToyMouse, int* hasLaser, int* hasScratcher, int* hasTower);
+void triggerMiniGame(int* mood);
 
 int main() {
     char name[20];
@@ -20,8 +21,11 @@ int main() {
     int mood = 3;
     int cat_x = HME_POS;
     int prevPos = -1;
-    int hasScratcher = 1;  // 테스트용
-    int hasTower = 1;      // 테스트용
+    int hasScratcher = 0;
+    int hasTower = 0;
+    int hasToyMouse = 0;
+    int hasLaser = 0;
+    int turnCount = 0;
 
     srand((unsigned int)time(NULL));
 
@@ -30,105 +34,52 @@ int main() {
     printf(" ( o.o )    \n");
     printf("  > ^ <     \n\n");
 
-    printf("야옹이의 이름을 지어 주세요: ");
+    printf("\n야옹이의 이름을 지어 주세요: ");
     scanf_s("%19s", name, (unsigned)_countof(name));
     printf("야옹이의 이름은 %s입니다.\n", name);
 
-    Sleep(2000);
+    Sleep(2500);
     system("cls");
 
     while (1) {
+        turnCount++;
+
+        if (turnCount % 3 == 0) {
+            triggerMiniGame(&mood);
+        }
+
         printGameState(name, soup_count, affinity, cp, mood);
         drawRoom(cat_x, prevPos, hasScratcher, hasTower);
 
-        prevPos = cat_x;
+        // ==================== [2-5 상호작용 입력 기능 추가] ====================
+        int userChoice = -1;
+        int menuNum = 0;
+        int optionMouse = -1;
+        int optionLaser = -1;
 
-        // 이동 처리 (간단히 랜덤 방향)
-        int dir = rand() % 3 - 1;  // -1, 0, +1
-        if (cat_x + dir >= HME_POS && cat_x + dir <= BWL_POS) {
-            cat_x += dir;
+        printf("어떤 상호작용을 하시겠습니까?\n");
+        printf("  %d. 아무것도 하지 않음\n", menuNum++);
+        printf("  %d. 긁어 주기\n", menuNum++);
+        if (hasToyMouse) {
+            optionMouse = menuNum;
+            printf("  %d. 장난감 쥐로 놀아 주기\n", menuNum++);
+        }
+        if (hasLaser) {
+            optionLaser = menuNum;
+            printf("  %d. 레이저 포인터로 놀아 주기\n", menuNum++);
         }
 
-        // 2-4 행동 처리 기능 추가
-        performAction(cat_x, &mood, &soup_count, &cp, name, hasScratcher, hasTower);
-
-        Sleep(2500);
-        system("cls");
+        while (1) {
+            printf(">> ");
+            if (scanf_s("%d", &userChoice) != 1 || userChoice < 0 || userChoice >= menuNum) {
+                printf("잘못된 입력입니다. 다시 입력해주세요.\n");
+                while (getchar() != '\n');
+            } else break;
+        }
+      
     }
 
     return 0;
-}
-
-// 2-4 행동 처리 기능 함수 정의
-void performAction(int cat_x, int* mood, int* soup_count, int* cp, const char* name, int hasScratcher, int hasTower) {
-    if (cat_x == HME_POS) {
-        if (*mood < 3) {
-            (*mood)++;
-            printf("집에서 쉰 %s의 기분이 좋아졌습니다: %d\n", name, *mood);
-        }
-    }
-    else if (hasTower && cat_x == 3) {
-        if (*mood < 2) (*mood) += 2;
-        else if (*mood == 2) (*mood)++;
-        printf("%s가 캣타워에서 놀아 기분이 좋아졌습니다.\n", name);
-    }
-    else if (hasScratcher && cat_x == 5) {
-        if (*mood < 3) {
-            (*mood)++;
-            printf("%s가 스크래처에서 놀아 기분이 살짝 좋아졌습니다.\n", name);
-        }
-    }
-    else if (cat_x == BWL_POS) {
-        int type = rand() % 3;
-        const char* names[] = { "감자 수프", "양송이 수프", "브로콜리 수프" };
-        printf("%s가 %s를 만들었습니다!\n", name, names[type]);
-        (*soup_count)++;
-        (*cp)++;
-    }
-}
-
-void printGameState(const char* name, int soup_count, int affinity, int cp, int mood) {
-    const char* moodText[] = {
-        "기분이 매우 나쁩니다.", "심심해합니다.",
-        "식빵을 굽습니다.", "골골송을 부릅니다."
-    };
-    const char* affinityText[] = {
-        "곁에 오는 것조차 싫어합니다.",
-        "간식 자판기 취급입니다.",
-        "그럭저럭 쓸만한 집사입니다.",
-        "훌륭한 집사로 인정 받고 있습니다.",
-        "집사 껌딱지입니다."
-    };
-
-    printf("=============== 현재 상태 ===============\n");
-    printf("현재까지 만든 수프: %d개\n", soup_count);
-    printf("CP: %d 포인트\n", cp);
-    printf("%s의 기분(0~3): %d\n", name, mood);
-    printf("기분 설명: %s\n", moodText[mood]);
-    printf("집사와의 관계(0~4): %d\n", affinity);
-    printf("  %s\n", affinityText[affinity]);
-    printf("=========================================\n");
-}
-
-void drawRoom(int cat_x, int prevPos, int hasScratcher, int hasTower) {
-    for (int i = 0; i < ROOM_WIDTH; i++) printf("#");
-    printf("\n#");
-    for (int i = 1; i < ROOM_WIDTH - 1; i++) {
-        if (i == HME_POS) printf("H");
-        else if (hasTower && i == 3) printf("T");
-        else if (hasScratcher && i == 5) printf("S");
-        else if (i == BWL_POS) printf("B");
-        else printf(" ");
-    }
-    printf("#\n#");
-    for (int i = 1; i < ROOM_WIDTH - 1; i++) {
-        if (i == cat_x) printf("C");
-        else if (i == prevPos && prevPos != cat_x) printf(".");
-        else printf(" ");
-    }
-    printf("#\n");
-    for (int i = 0; i < ROOM_WIDTH; i++) printf("#");
-    printf("\n\n");
 }
 
 
