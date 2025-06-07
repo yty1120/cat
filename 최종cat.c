a98a5d1a361bfa616ad5b794cf9dab61ec5b0934
yty1120 <yty8119@gmail.com>
2025-06-08 06:30:42 +0900

    2-3 이동기능 수정
    + 랜덤 방향

--- 최종cat.c
+++ 최종cat.c
@@ -1,45 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
-#include <windows.h> // 윈도우 전용
+#include <windows.h> 
 
 #define ROOM_WIDTH 15
 #define HME_POS 1
 #define BWL_POS (ROOM_WIDTH - 2)
 
-//  상태창 출력 기능
-void printGameState(int soup_count, int affinity, int mood);
-
-//  방 구조 출력 기능
-void drawRoom(int cat_x);
-
-//  이동 시스템 (기분에 따라 이동 처리)
-void updateCatPosition(int* cat_x, int mood);
+void printGameState(const char* name, int soup_count, int affinity, int cp, int mood);
+void drawRoom(int cat_x, int prevPos, int hasScratcher, int hasTower);
+void performAction(int cat_x, int* mood, int* soup_count, int* cp, const char* name, int hasScratcher, int hasTower); // 행동 처리
 
 int main() {
-    int soup_count = 0;    // 만든 수프 개수
-    int affinity = 2;      // 집사와의 친밀도
-    int mood = 3;          // 고양이 기분 (0~3)
-    int cat_x = HME_POS;   // 고양이 위치
+    char name[20];
+    int soup_count = 0;
+    int affinity = 2;
+    int cp = 0;
+    int mood = 3;
+    int cat_x = HME_POS;
+    int prevPos = -1;
+    int hasScratcher = 1;  // 테스트용
+    int hasTower = 1;      // 테스트용
 
     srand((unsigned int)time(NULL));
 
+    printf("****야옹이와 수프****\n\n");
+    printf("  /\\_/\\     \n");
+    printf(" ( o.o )    \n");
+    printf("  > ^ <     \n\n");
+
+    printf("야옹이의 이름을 지어 주세요: ");
+    scanf_s("%19s", name, (unsigned)_countof(name));
+    printf("야옹이의 이름은 %s입니다.\n", name);
+
+    Sleep(2000);
+    system("cls");
+
     while (1) {
-        //  기분 시스템 - 주사위로 기분 변화
-        int moodRoll = rand() % 6 + 1;
-        int threshold = 6 - affinity;
-        printf("주사위를 굴립니다... %d 나옴\n", moodRoll);
+        printGameState(name, soup_count, affinity, cp, mood);
+        drawRoom(cat_x, prevPos, hasScratcher, hasTower);
 
-        if (moodRoll <= threshold && mood > 0) {
-            mood--;
-            printf("기분이 나빠졌습니다! 현재 기분: %d\n", mood);
+        prevPos = cat_x;
+
+        // 이동 처리 (간단히 랜덤 방향)
+        int dir = rand() % 3 - 1;  // -1, 0, +1
+        if (cat_x + dir >= HME_POS && cat_x + dir <= BWL_POS) {
+            cat_x += dir;
         }
-
-        printGameState(soup_count, affinity, mood); // 상태 출력
-        drawRoom(cat_x);                            // 방 출력
-
-        // 🐾 고양이 이동 시스템 적용
-        updateCatPosition(&cat_x, mood);
 
         Sleep(2500);
         system("cls");
@@ -48,13 +56,12 @@ int main() {
     return 0;
 }
 
-//  상태창 출력 기능
-void printGameState(int soup_count, int affinity, int mood) {
+
+
+void printGameState(const char* name, int soup_count, int affinity, int cp, int mood) {
     const char* moodText[] = {
-        "기분이 매우 나쁩니다.",
-        "심심해합니다.",
-        "식빵을 굽습니다.",
-        "골골송을 부릅니다."
+        "기분이 매우 나쁩니다.", "심심해합니다.",
+        "식빵을 굽습니다.", "골골송을 부릅니다."
     };
     const char* affinityText[] = {
         "곁에 오는 것조차 싫어합니다.",
@@ -66,24 +73,28 @@ void printGameState(int soup_count, int affinity, int mood) {
 
     printf("=============== 현재 상태 ===============\n");
     printf("현재까지 만든 수프: %d개\n", soup_count);
-    printf("집사와의 관계(0~4): %d - %s\n", affinity, affinityText[affinity]);
-    printf("고양이의 기분(0~3): %d - %s\n", mood, moodText[mood]);
+    printf("CP: %d 포인트\n", cp);
+    printf("%s의 기분(0~3): %d\n", name, mood);
+    printf("기분 설명: %s\n", moodText[mood]);
+    printf("집사와의 관계(0~4): %d\n", affinity);
+    printf("  %s\n", affinityText[affinity]);
     printf("=========================================\n");
 }
 
-//  방 구조 출력 기능
-void drawRoom(int cat_x) {
-    printf("\n");
+void drawRoom(int cat_x, int prevPos, int hasScratcher, int hasTower) {
     for (int i = 0; i < ROOM_WIDTH; i++) printf("#");
     printf("\n#");
     for (int i = 1; i < ROOM_WIDTH - 1; i++) {
         if (i == HME_POS) printf("H");
+        else if (hasTower && i == 3) printf("T");
+        else if (hasScratcher && i == 5) printf("S");
         else if (i == BWL_POS) printf("B");
         else printf(" ");
     }
     printf("#\n#");
     for (int i = 1; i < ROOM_WIDTH - 1; i++) {
         if (i == cat_x) printf("C");
+        else if (i == prevPos && prevPos != cat_x) printf(".");
         else printf(" ");
     }
     printf("#\n");
@@ -91,15 +102,5 @@ void drawRoom(int cat_x) {
     printf("\n\n");
 }
 
-//  고양이 이동 시스템
-void updateCatPosition(int* cat_x, int mood) {
-    if (mood == 0 && *cat_x > HME_POS) {
-        (*cat_x)--; // 기분이 매우 나쁠 때 집 방향으로 이동
-    } else if (mood == 3 && *cat_x < BWL_POS) {
-        (*cat_x)++; // 기분이 좋을 때 수프 방향으로 이동
-    } else {
-        // 기분이 1~2일 때는 이동 없음
-    }
-}
 
 
